7e55fdb27f148c842079602650e485357f899c12
ThePepidev <mathys1.dupont@epitech.eu>
2025-02-06 10:23:14 +0100

    fix(hash.c): fixing the hash function

--- src/hash.c
+++ src/hash.c
@@ -5,35 +5,15 @@
 ** hash.c
 */
 
-#include "secured.h"
-
-static int len_nbr(unsigned long number)
-{
-    int len = 0;
-
-    if (number == 0)
-        return 1;
-    while (number >= 1) {
-        number /= 10;
-        len++;
-    }
-    return len;
-}
-
+#include "../include/secured.h"
+#include <string.h>
 int hash(char *key, int len)
 {
-    unsigned long temp = 4523;
-    long hash = 4523;
+    unsigned long hash = 5381;
 
-    if (!key || !len)
-        return 84;
     for (int i = 0; key[i]; i++)
         hash = ((hash << 5) + hash) + key[i];
-    while (len_nbr(hash) < len)
-        hash += temp + (hash / 2);
-    while (len_nbr(hash) > len)
-        hash -= temp + (hash / 2);
-    if (hash < 0)
-        hash = -hash;
-    return hash;
+    hash = (hash * len) + len;
+    hash = hash % __INT32_MAX__;
+    return (int)hash;
 }
