6a55ae4faa3958a2b6ab084811b24317bcf8edfd
ThePepidev <mathys1.dupont@epitech.eu>
2025-02-06 10:22:24 +0100

    feat(secured.h): add prototype of search.c

--- include/secured.h
+++ include/secured.h
@@ -11,19 +11,20 @@
     #include "my.h"
     #include <unistd.h>
 
-    typedef struct node_s {
-        char *key;
-        char *value;
-        int hash;
-        struct node_s *next;
-    } node_t;
+typedef struct node_s {
+    char *key;
+    char *value;
+    int hash;
+    struct node_s *next;
+} node_t;
 
-    typedef struct hashtable_s {
-        node_t **tab;
-        int (*hash)(char *, int);
-        int len_hashtable;
-    } hashtable_t;
+typedef struct hashtable_s {
+    node_t **tab;
+    int (*hash)(char *, int);
+    int len_hashtable;
+} hashtable_t;
 
 int ht_insert(hashtable_t *ht, char *key, char *value);
 int hash(char *key, int len);
+char *ht_search(hashtable_t *ht, char *key);
 #endif
